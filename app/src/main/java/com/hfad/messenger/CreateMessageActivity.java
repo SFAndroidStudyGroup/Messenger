7bbc9c2752cba1ebbbfd156092b3419bf035e961
RyanSong <shoidoly75@gmail.com>
2017-01-18 17:57:45 -0600

    Initial commit

---
+++ app/src/main/java/com/hfad/messenger/CreateMessageActivity.java
@@ -0,0 +1,23 @@
+package com.hfad.messenger;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class CreateMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_message);
+    }
+
+    public void onSendMessage(View view){
+        Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        EditText messageView = (EditText)findViewById(R.id.message);
+        intent.putExtra(ReceiveMessageActivity.MESSAGE, messageView.getText().toString());
+        startActivity(intent);
+    }
+}
